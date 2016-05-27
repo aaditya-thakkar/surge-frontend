c72b77c749eae5c20041af5d6d6b46204459a559
Yash Shah <mail@yashshah.com>
2016-05-27 17:33:04 +0530

    Adding buildRequestObject in helper function

--- src/helper.js
+++ src/helper.js
@@ -2,10 +2,30 @@ var Appbase = require('../node_modules/appbase-js');
 var config = require('../config.json');
 
 module.exports = {
-  appbaseRef : new Appbase({
+  appbaseRef: new Appbase({
     url: 'https://scalr.api.appbase.io',
     appname: config.appbase.appname,
     username: config.appbase.username,
     password: config.appbase.password
-  })
+  }),
+  buildRequestObject: function(long, lat) {
+    return ({
+      type: config.appbase.type,
+      body: {
+        "query": {
+          "filtered": {
+            "query": {
+              "match_all": {}
+            },
+            "filter": {
+              "geo_distance": {
+                "distance": "20km",
+                "location": [long, lat]
+              }
+            }
+          }
+        }
+      }
+    })
+  } 
 };
