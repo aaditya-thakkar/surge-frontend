b39b5f92b024d57ad676c3a323d8a4f9398dd7d9
aaditya-thakkar <thakkar.aadi1@gmail.com>
2016-07-01 19:41:03 +0530

    changed the simulator

--- backend/simulator.js
+++ backend/simulator.js
@@ -5,13 +5,12 @@ var helper = require('../src/helper.js');
 var appbaseRef = helper.appbaseRef;
 
 var maxNumberOfNodes = 10;
-var timeBetweenInsertions = 500;
+var timeBetweenInsertions = 1000;
 
 // enter demander's location into appbase table
-  function addNode(index, type) {
+  function addNode(index) {
     var latLongData = {
-      object_type: type,
-      location: [parseFloat(locationGenerator.generateLatLong().long), parseFloat(locationGenerator.generateLatLong().lat)]
+      "location-field": [parseFloat(locationGenerator.generateLatLong().long), parseFloat(locationGenerator.generateLatLong().lat)]
     };
     var requestObject = {
       type: config.appbase.type,
@@ -22,7 +21,7 @@ var timeBetweenInsertions = 500;
     appbaseRef.index(requestObject).on('data', function(response) {
       console.log(" Inserted ", index);
       setTimeout(deleteNode(index), (index + maxNumberOfNodes) * timeBetweenInsertions);
-      setTimeout(generateNode(index), (index + maxNumberOfNodes) * timeBetweenInsertions);
+      setTimeout(addNode(index), (index + maxNumberOfNodes) * timeBetweenInsertions);
     }).on('error', function(error) {
       console.log(error);
     });
@@ -41,20 +40,11 @@ var timeBetweenInsertions = 500;
     });
   }
 
-  function generateNode(index) {
-    var weight = Math.random();
-    if (weight > 0.5){
-      addNode(index, 'demander');
-    }
-    else
-      addNode(index, 'supplier');
-  }
-
 module.exports = {
   // Randomly generate the demander & Supplier
   dataGenerator: function() {
     for (var index = 0; index <= maxNumberOfNodes; index++) {
-      setTimeout(generateNode(index), index * timeBetweenInsertions);
+      setTimeout(addNode(index), index * timeBetweenInsertions);
     }
   }
 }
