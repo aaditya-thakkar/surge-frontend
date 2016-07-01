e1bc1a04d8fce4d24b501e5c94603e1ec8d821ea
aaditya-thakkar <thakkar.aadi1@gmail.com>
2016-07-01 19:38:56 +0530

    merged heatmap creator and grid creator

--- src/HeatmapCreator.js
+++ src/HeatmapCreator.js
@@ -1,8 +1,73 @@
-module.exports ={
+module.exports = {
+  /* Create grid bounds on the map, called from Map.js on map initialization. */
+  createGridLines: function(mapBounds, opacity) {
+    var llOffset =  0.00666666666666667*1.5;
+    var gridCenterPointsArray = [];
 
-  createHeatmap: function(opacity, color, upLeftCoord, upRightCoord, lowRightCoord, lowLeftCoord) {
+    // north, south, east, and west coordinates of the map.
+    var north = mapBounds.getNorthEast().lat();
+    var south = mapBounds.getSouthWest().lat();
+    var east = mapBounds.getNorthEast().lng();
+    var west = mapBounds.getSouthWest().lng();
 
-    // bounds for the heatmap polygon
+    // defines the size of the grid sides.
+    var topLat = Math.ceil(north / llOffset) * llOffset;
+    var rightLong = Math.ceil(east / llOffset) * llOffset;
+    var bottomLat = Math.floor(south / llOffset) * llOffset;
+    var leftLong = Math.floor(west / llOffset) * llOffset;
+
+    // generates each grid's coordinates
+    for (var latitude = bottomLat; latitude <= topLat+3*llOffset; latitude += llOffset) {
+      if(latitude>=topLat+llOffset){
+        leftLong+=2*llOffset;
+        rightLong-=llOffset;
+      }
+      for(var longitude = leftLong+llOffset; longitude<= rightLong; longitude += llOffset) {
+        var upLeftCoord= {
+          lat: latitude + llOffset/2,
+          lng: longitude - llOffset/2
+        };
+        var upRightCoord= {
+          lat: latitude + llOffset/2,
+          lng: longitude + llOffset/2
+        };
+        var lowLeftCoord= {
+          lat: latitude - llOffset/2,
+          lng: longitude - llOffset/2
+        };
+        var lowRightCoord= {
+          lat: latitude - llOffset/2,
+          lng: longitude + llOffset/2
+        };
+
+        // initial default color when map loads and grids are created
+        var color = "#00ffffff";
+        var opacity = opacity;
+
+        // grid object representing its center and other properties
+        var gridCenterObject = {
+          lat: latitude,
+          long: longitude,
+          location: new google.maps.LatLng(latitude, longitude),
+          upLeftCoord: upLeftCoord,
+          upRightCoord: upRightCoord,
+          lowLeftCoord: lowLeftCoord,
+          lowRightCoord: lowRightCoord,
+          surgePrice: 0.0,
+          numberOfDemanders: 0,
+          numberOfSuppliers: 0,
+          color: color,
+          opacity: opacity,
+          cell: this.createCell(opacity, color, upLeftCoord, upRightCoord, lowRightCoord, lowLeftCoord)
+        };
+        gridCenterPointsArray.push(gridCenterObject);
+      }
+    }
+    return gridCenterPointsArray;
+  },
+
+  createCell: function(opacity, color, upLeftCoord, upRightCoord, lowRightCoord, lowLeftCoord) {
+    // bounds for the cell polygon
     var polyCoords = [
       upLeftCoord,
       upRightCoord,
@@ -10,8 +75,8 @@ module.exports ={
       lowLeftCoord
     ];
 
-    // heatmap properties
-    var heatmapProps = new google.maps.Polygon({
+    // cell properties
+    var cellProps = new google.maps.Polygon({
       paths: polyCoords,
       strokeColor: color,
       strokeOpacity: opacity,
@@ -19,6 +84,6 @@ module.exports ={
       fillColor: color,
       fillOpacity: opacity
     });
-    return heatmapProps;
+    return cellProps;
   }
-};
+}
