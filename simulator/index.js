ccb406cd4d906e44703a98d9b0b263320999fd9e
aaditya-thakkar <thakkar.aadi1@gmail.com>
2016-07-02 00:16:12 +0530

    added deletion of data

--- simulator/index.js
+++ simulator/index.js
@@ -4,50 +4,53 @@ var config = require('../config.json');
 var helper = require('../src/helper.js');
 var appbaseRef = helper.appbaseRef;
 
-var maxNumberOfNodes = 100;
+var maxNumberOfNodes = 10;
 var timeBetweenInsertions = 1000;
 
 // enter demander's location into appbase table
-  function addNode(index) {
-    var latLongData = {
-      "location-field": [parseFloat(locationGenerator.generateLatLong().long), parseFloat(locationGenerator.generateLatLong().lat)]
-    };
-    var requestObject = {
-      type: config.appbase.type,
-      id: index.toString(),
-      body: latLongData,
-    };
-    // appbase index query
-    appbaseRef.index(requestObject).on('data', function(response) {
-      console.log(" Inserted ", index);
-      // setTimeout(deleteNode(index), (index + maxNumberOfNodes) * timeBetweenInsertions);
-      // setTimeout(addNode(index), (index + maxNumberOfNodes) * timeBetweenInsertions);
-    }).on('error', function(error) {
-      console.log(error);
-    });
-  }
+function addNode(index) {
+  var latLongData = {
+    "location-field": [parseFloat(locationGenerator.generateLatLong().long), parseFloat(locationGenerator.generateLatLong().lat)]
+  };
+  var requestObject = {
+    type: config.appbase.type,
+    id: index.toString(),
+    body: latLongData,
+  };
+  // appbase index query
+  appbaseRef.index(requestObject).on('data', function(response) {
+    console.log(" Inserted ", index);
+     setTimeout(deleteNode(index), (index + maxNumberOfNodes) * timeBetweenInsertions);
+     setTimeout(addNode(index), (index + maxNumberOfNodes) * timeBetweenInsertions);
+  }).on('error', function(error) {
+    console.log(error);
+  });
+}
 
-  // delete demander's location from appbase table
-  function deleteNode(index) {
-    var requestObject = {
-      type: config.appbase.type,
-      id: index.toString()
-    };
-    appbaseRef.delete(requestObject).on('data', function(response) {
-       console.log(" Deleted ", index);
-    }).on('error', function(error) {
-      console.log(error);
-    });
-  }
+// delete demander's location from appbase table
+function deleteNode(index) {
+  var requestObject = {
+    type: config.appbase.type,
+    id: index.toString()
+  };
+  appbaseRef.delete(requestObject).on('data', function(response) {
+    console.log(" Deleted ", index);
+  }).on('error', function(error) {
+    console.log(error);
+  });
+}
+
+function generateData(index) {
+  setTimeout(function(){
+    addNode(index)
+  }, index * timeBetweenInsertions);
+}
 
 module.exports = {
   // Randomly generate the demander & Supplier
   dataGenerator: function() {
     for (var index = 1; index <= maxNumberOfNodes; index++) {
-      setTimeout(function(){
-        addNode(index)
-      }
-      , index * timeBetweenInsertions);
+      generateData(index);
     }
   }
 }
