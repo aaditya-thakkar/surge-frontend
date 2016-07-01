9fae5f45b63c28f086988a7fce88d93e60e14bed
aaditya-thakkar <thakkar.aadi1@gmail.com>
2016-07-01 19:42:27 +0530

    removed suppliers from indexing and calculating surge

--- src/Evaluator.js
+++ src/Evaluator.js
@@ -2,49 +2,31 @@ module.exports = {
 
   findSurgePrice: function(stream, gridCenterPoints, index) {
     if(stream._deleted == true){
-      if(stream._source.object_type == "demander"){
         gridCenterPoints[index].numberOfDemanders--;
-      }
-      else if(stream._source.object_type == "supplier"){
-        gridCenterPoints[index].numberOfSuppliers--;
-      }
     }
     else {
-      if(stream._source.object_type == "demander"){
         gridCenterPoints[index].numberOfDemanders++;
-      }
-      else if(stream._source.object_type == "supplier"){
-        gridCenterPoints[index].numberOfSuppliers++;
-      }
     }
 
     // surge price = number of demanders/number of suppliers
-    gridCenterPoints[index].surgePrice = gridCenterPoints[index].numberOfDemanders/gridCenterPoints[index].numberOfSuppliers;
-
-    // corner cases
-    if(gridCenterPoints[index].numberOfDemanders != 0 && gridCenterPoints[index].numberOfSuppliers == 0){
-      gridCenterPoints[index].surgePrice = 7;
-    }
-    else if(gridCenterPoints[index].numberOfDemanders == 0 && gridCenterPoints[index].numberOfSuppliers == 0){
-      gridCenterPoints[index].surgePrice = 0;
-    }
+    gridCenterPoints[index].surgePrice = gridCenterPoints[index].numberOfDemanders;
 
     // colors and labels according to the surge price measures
-    if(gridCenterPoints[index].surgePrice <= 0.6){
+    if(gridCenterPoints[index].surgePrice <= 1){
       gridCenterPoints[index].color = "#00ffffff";
       gridCenterPoints[index].opacity = 0.0;
     }
-    else if(gridCenterPoints[index].surgePrice <= 0.75){
+    else if(gridCenterPoints[index].surgePrice <= 2){
       gridCenterPoints[index].color = "#ec891d";
-      gridCenterPoints[index].opacity = 0.35;
+      gridCenterPoints[index].opacity = 0.15;
     }
-    else if(gridCenterPoints[index].surgePrice <= 2.0){
+    else if(gridCenterPoints[index].surgePrice <= 3){
       gridCenterPoints[index].color = "#ff0000";
-      gridCenterPoints[index].opacity = 0.35;
+      gridCenterPoints[index].opacity = 0.15;
     }
     else{
       gridCenterPoints[index].color = "#4c0000";
-      gridCenterPoints[index].opacity = 0.35;
+      gridCenterPoints[index].opacity = 0.15;
     }
 
     return ({
